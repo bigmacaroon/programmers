34a4f3e09c0a2ce76a3f448f52bf79351a1b5fcd
bigmacaroon <60723756+bigmacaroon@users.noreply.github.com>
2020-02-11 23:06:25 +0900

    Create subak.java

---
+++ subak.java
@@ -0,0 +1,23 @@
+class Solution {
+	  public static String solution(int n) {
+	    
+		  String su = "수";
+		  String bak = "박";
+		  String answer = "";
+		  String temp = "";
+		for(int i=0 ; i < n/2; i++) {
+			temp = su ;
+			temp += bak ;
+			answer += temp;			
+		}
+		if(n%2 != 0) {
+			answer += su;
+		}
+		  
+	      return answer;
+	  }
+	  public static void main(String[]args) {
+		  
+		  System.out.println(solution(7));
+	  }
+	}
